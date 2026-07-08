89ff986122164c2ecee23fd01113f660e0552d53
syafainesya <syafainesyaa@gmail.com>
2026-07-08 08:54:41 +0700

    main screen

--- lib/screens/auth/register_screen.dart
+++ lib/screens/auth/register_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../core/themes/app_theme.dart';
-import '../../widgets/common/app_button.dart';
+// import '../../widgets/common/app_button.dart';
 import '../../widgets/common/app_input.dart';
-import '../../widgets/common/loading_widget.dart';
-import '../../widgets/common/error_widget.dart';
+// import '../../widgets/common/loading_widget.dart';
+// import '../../widgets/common/error_widget.dart';
 
 /// Register Screen
 class RegisterScreen extends ConsumerStatefulWidget {
